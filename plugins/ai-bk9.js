672db02979c0865acb1933fd2f291fbffdec7381
HardyOfc <aymentz04@gmail.com>
2024-07-11 13:31:40 +0100

    🇲🇦 𝙷𝙰𝚁𝙳𝚈 - 𝙱𝙾𝚃 - 𝙼𝙳 👑

--- plugins/ai-bk9.js
+++ plugins/ai-bk9.js
@@ -8,23 +8,7 @@ const handler = async (m, {conn, text, command}) => {
   const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`));
   const tradutor = _translate.BK9.BK9;
 
-  if (command === 'bk9dalle') {
-    if (!text) throw `${tradutor.bk9dalletext}`;
-
-    await conn.sendMessage(m.chat, {text: tradutor.bk9dallewait}, {quoted: m});
-
-    try {
-      const BK9 = `https://api.bk9.site/ai/photoleap?q=${encodeURIComponent(text)}`;
-      const response = await fetch(BK9);
-      const result = await response.json();
-
-      if (result.status) {
-        await conn.sendMessage(m.chat, {image: {url: result.BK9}}, {quoted: m});
-      }
-    } catch (error) {
-      throw `${tradutor.bk9dalleerr}`;
-    }
-  } else if (command === 'bk9') {
+  if (command === 'gpt4') {
     if (!text) throw `${tradutor.bk9text}`;
 
     try {
@@ -41,7 +25,24 @@ const handler = async (m, {conn, text, command}) => {
     } catch (error) {
       throw `${tradutor.bk9err}`;
     }
-  } else if (command === 'bk9img') {
+  } else if (command === 'gemini') {
+    if (!text) throw `${tradutor.bk9text}`;
+
+    try {
+      conn.sendPresenceUpdate('composing', m.chat);
+      const BK9api = `https://api.bk9.site/ai/gemini?q=${encodeURIComponent(text)}`;
+      const BK99 = await fetch(BK9api);
+      const BK8 = await BK99.json();
+      if (BK8.status && BK8.BK9) {
+        const respuestaAPI = BK8.BK9;
+        conn.reply(m.chat, respuestaAPI, m);
+      } else {
+        throw `${tradutor.bk9err}`;
+      }
+    } catch (error) {
+      throw `${tradutor.bk9err}`;
+    }
+  } else if (command === 'geminiimg') {
     let BK7 = m.quoted ? m.quoted : m;
     let BK8 = (BK7.msg || BK7).mimetype || BK7.mediaType || '';
     if (/image/g.test(BK8) && !/webp/g.test(BK8)) {
@@ -55,6 +56,6 @@ const handler = async (m, {conn, text, command}) => {
   }
 };
 
-handler.command = ['bk9dalle', 'bk9', 'bk9img'];
+handler.command = ['gpt4', 'gemini', 'geminiimg'];
 handler.tags = ['ai'];
 export default handler;
